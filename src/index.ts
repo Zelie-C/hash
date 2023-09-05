5544f69a88bf61feef5547b1d597be4bbc0c364c
Zelie-C <zelie.chemin@gmail.com>
2023-09-05 13:38:22 +0200

    fonction création dossiers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import {MD5} from 'crypto-js';
+import { writeFile } from 'fs';
 import { mkdirSync} from 'fs-extra';
 
 function hashing (string: string) {
     let hash = MD5(string);
-    console.log(hash.toString());
     return hash;
 }
 
 hashing("ehhedad");
 
+function createDirectories (n: number) {
+    mkdirSync('../jeu')
+    for (let i = 0; i <= n; i++){
+        mkdirSync(`../jeu/${hashing(`Dossier${i}`)}`);
+}
+}
+
+createDirectories(2);
+
+// for (let j = 0; j <= nbfiles; j++){
+
+//     writeFile(`../jeu/${`Dossier${Math.floor(Math.random(i))}`}/index.md`, )
+// }
